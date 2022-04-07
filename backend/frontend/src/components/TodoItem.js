ebd29bc7ecffe7f0a52c05aa52848aa8bdf0eb39
Dmitriy Meshkov <Flasher9595@yandex.ru>
2022-04-07 20:49:42 +0300

    added 11 HW (Работа с формами. Сборка проекта для production)

--- backend/frontend/src/components/TodoItem.js
+++ backend/frontend/src/components/TodoItem.js
@@ -1,11 +1,15 @@
-const TodoItem = ({todo, users}) => {
+import React from 'react'
+
+const TodoItem = ({todo, users, deleteTodo}) => {
     let user = users.filter((item) => item.id == todo.user)[0]
     return (
         <tr>
             <td>{todo.project}</td>
-            <td>{user.firstName}</td>
+            <td>{user.username}</td>
             <td>{todo.title}</td>
             <td>{todo.text}</td>
+            <td>{todo.isActive ? '✅' : '❌'}</td>
+            {deleteTodo && <td><button onClick={()=>deleteTodo(todo.id)} type='button'>{todo.isActive ? 'Delete ❌ ': 'Activate ✅ ' }</button></td>}
         </tr>
     )
 }
