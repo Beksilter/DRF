f1227da7cb4bdfa90765a291c37f8dfe6fbff1d8
Dmitriy Meshkov <Flasher9595@yandex.ru>
2022-04-13 14:52:16 +0300

    added 12 HW (Развертывание проекта с Docker и docker-compose)

--- backend/todo/settings/base.py
+++ backend/todo/settings/base.py
@@ -17,11 +17,11 @@ from datetime import datetime, timedelta      # для JSON Web Token (сокр.
 from django.conf import settings    # для JWT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-from dotenv import load_dotenv
-
-load_dotenv(BASE_DIR / '.env')
+# from dotenv import load_dotenv
+#
+# load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend/build'],
+        'DIRS': [BASE_DIR / '..' / 'frontend' / 'build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,7 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR / 'frontend/build/static',)
+STATICFILES_DIRS = (
+    BASE_DIR / '..' / 'frontend' / 'build' / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
